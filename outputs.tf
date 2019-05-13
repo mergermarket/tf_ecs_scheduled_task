82e407d45a42ee11ef91d8b4cb12dfdbc38c519b
Keir Badger <keir.badger@acuris.com>
2019-05-13 16:44:52 +0100

    Add outputs for the task roles

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "task_role_arn" {
+  value = "${module.taskdef.task_role_arn}"
+}
+
+output "task_role_name" {
+  value = "${module.taskdef.task_role_name}"
+}
+
+output "task_execution_role_arn" {
+  value = "${module.taskdef.tasks_execution_role_arn}"
+}
+
+output "task_execution_role_name" {
+  value = "${module.taskdef.tasks_execution_role_name}"
+}
