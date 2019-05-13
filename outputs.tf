f939fd3f258108b7b5f31704c800c3aeea98b569
Keir Badger <keir.badger@acuris.com>
2019-05-13 17:02:02 +0100

    Correct name of output value

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,9 @@ output "task_role_name" {
 }
 
 output "task_execution_role_arn" {
-  value = "${module.taskdef.tasks_execution_role_arn}"
+  value = "${module.taskdef.task_execution_role_arn}"
 }
 
 output "task_execution_role_name" {
-  value = "${module.taskdef.tasks_execution_role_name}"
+  value = "${module.taskdef.task_execution_role_name}"
 }
